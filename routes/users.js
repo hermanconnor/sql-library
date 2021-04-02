1192eda250e9971b656e9de77e71b0ceba180a92
Herman <hermanconnor11@gmail.com>
2021-04-01 20:05:23 -0400

    Renders index but not others

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var { Book } = require('../models');
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+/* HANDLER FUNCTION TO WRAP EACH ROUTE */
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (error) {
+      // Forward error to the global error handler
+      next(error);
+    }
+  };
+}
+
+/* GET books listing. */
+router.get('/', (req, res) => {
+  res.redirect('../views/books');
 });
+
+router.get(
+  '/books',
+  asyncHandler(async (req, res) => {
+    const books = await Book.findAll();
+    res.render('../views/books', { books, title: 'Books' });
+  })
+);
 
 module.exports = router;
